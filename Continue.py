67a557c6c05d5b878e8729aaa627736f33f9462d
AbdulWassay2013 <nadeemalbaraka@gmail.com>
2025-09-14 10:44:32 +0500

    Keywords in python

---
+++ Continue.py
@@ -0,0 +1,8 @@
+x = 0
+while x < 10:
+    x = x + 1
+    if x == 5:
+        continue
+    print("Current Value of x",x)
+print("Good Bye")
+
---
+++ Practice 2.py
@@ -0,0 +1,9 @@
+for x in range(31):
+    if x % 3 == 0:
+        print("it is divided by 3")
+    elif x % 5 == 0:
+        print("It is divided by 5")
+    else:
+        print(False)
+
+    
